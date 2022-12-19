141f058f4b470770736c2e8d716507e86142c9a2
DHIRAJ SINGHA <76531276+dhirajsingha143@users.noreply.github.com>
2022-12-19 13:47:23 +0100

    Add JS for the mobile-menu navigation

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('hello wolrd');
+const divMenu = document.querySelector('.nav-menu');
+const crossButton = document.querySelector('.cross-button');
+const hamburgerButton = document.querySelector('.fa-bars');
+
+const openMenu = () => {
+    divMenu.classList.toggle('show');
+};
+
+crossButton.addEventListener('click', () => {
+    openMenu();
+});
+
+hamburgerButton.addEventListener('click', () => {
+    openMenu();
+});
+
+$('.nav-menu li a').on('click', () => {
+    $('#cross-button').click();
+});
