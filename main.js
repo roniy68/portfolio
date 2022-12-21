6f9cb90812a085d561f996b2b333c37dbc287bdf
DHIRAJ SINGHA <76531276+dhirajsingha143@users.noreply.github.com>
2022-12-21 14:16:39 +0100

    Add JS script for project-cards and popup-cards

--- main.js
+++ main.js
@@ -18,3 +18,219 @@ hamburgerButton.addEventListener('click', () => {
 navLink.addEventListener('click', () => {
   openMenu();
 });
+
+function showDetails(project) {
+  //container for the popup-window
+  const popupContainer = document.createElement('div');
+  popupContainer.classList.add('popup-container');
+  popupContainer.id = 'popup-contianer';
+  const popupImgContainer = document.createElement('div');
+  popupImgContainer.classList.add('popupimg-container');
+
+  //img
+  const imgDiv = document.createElement('div');
+  imgDiv.classList.add('image-container');
+  const img = document.createElement('img');
+  img.classList.add('img');
+  img.scr = project.image;
+  img.alt = 'Portfolio Project Image';
+  imgDiv.append(img);
+  //div.image-container>
+  //img.img>scr="project.image">alt="Portfolio Project Image"
+
+  //Title
+  const title = document.createElement('h2');
+  title.classList.add('project-title');
+  title.textContent = project.name;
+  //h2.project-title> project.name
+
+  //btnClose
+  const btnContainer = document.createElement('div');
+  btnContainer.classList.add('btn-container');
+  const btnClose = document.createElement('img');
+  btnClose.id = "close-btn";
+  btnClose.src = 'images/cross2.svg';
+  btnClose.alt = 'Close button';
+  btnClose.addEventListener(click, () => {
+    document.getElementById('popup-container').remove();
+  });
+  btnContainer.append(btnClose);
+  //div.btn-container> img#close-btn>src="images/cross2.svg">alt...
+  //event = btnClose-> #popup-container.remove()
+
+// --------------------------------------------------------
+
+  // Descriptions
+  const description = document.createElement('p');
+  description.classList.add('description');
+  description.textContent = project.description;
+  //p.discription <- project.description
+
+  // detail Footer
+  const detailFooter = document.createElement('div');
+  detailFooter.classList.add('detail-footer');
+  //div.detail-footer
+
+  //Creating list of programming languages
+  const technologies = document.createElement('ul');
+  technologies.classList.add('tag');
+  for(let i = 0 ; i < project.technologies.length; i +=1){
+    const list = document.createElement('li');
+    list.classList.add('lang');
+    list.textContent = project.technologies[i];
+    technologies.append(list);
+  }
+  //ul.tag > li.lang> <- project.technologies[i]
+
+  // Buttons
+  const buttonLive = document.createElement('button');
+  buttonLive.classList.add('btn');
+  buttonLive.type = 'button';
+  //button.btn>type="button"
+
+  const txtBtnLive = document.createElement('p');
+  txtBtnLive.textContent = 'See Live';
+  //p <- "See Live"
+
+  const imgBtnLive = document.createElement('img');
+  imgBtnLive.src = 'images/live-Icon.svg';
+  imgBtnLive.alt = 'Button Live';
+  buttonLive.append(txtBtnLive);
+  buttonLive.append(imgBtnLive);
+  //button.btn>type="button">p <- "See Live">img>src..>alt..
+
+  const buttonSource = document.createElement('button');
+  buttonSource.classList.add('btn');
+  buttonSource.type = 'button';
+
+  const txtBtnSource = document.createElement('p');
+  txtBtnSource.textContent = 'See Source';
+
+  const imgBtnSource = document.createElement('img');
+  imgBtnSource.src = 'images/purple-github in button.svg';
+  imgBtnSource.alt = 'Button Source';
+  buttonSource.append(txtBtnSource);
+  buttonSource.append(imgBtnSource);
+  //button.btn>type="button">p <- "See Source">img>src..>alt..
+
+  detailFooter.append(technologies);
+  detailFooter.append(buttonLive);
+  detailFooter.append(buttonSource);
+  //div.detail-footer >
+    //ul.classification > li.lang> a>href="#" <- project.technologies[i]
+    //button.btn>type="button">p <- "See Live">img>src..>alt..
+    //button.btn>type="button">p <- "See Source">img>src..>alt..
+
+  // Add all element into the container
+  popupImgContainer.append(title);
+  popupImgContainer.append(btnContainer);
+  popupImgContainer.append(projectName);
+  popupImgContainer.append(imgDiv);
+  popupImgContainer.append(description);
+  popupImgContainer.append(detailFooter);
+
+  popupContainer.append(popupImgContainer);
+  document.body.append(popupContainer);
+
+  //All elements append sequence wise inside body. 
+
+  /*popupImgContainer.popupimg-container
+    h2.project-title> project.name
+    div.btn-container> img#close-btn>src="images/cross2.svg">alt...{EVENT}
+        ul.project-name
+            li.sub-title> a>href='#'<- project.details[j] {Cannopy}
+            li.bullet(&#x2022) {*}
+            li> a.backend-yr>href="#"<- project.details[j] {Backend Dev}
+            li.bullet(&#x2022) {*}
+            li> a.backend-yr <-project.details[j] {2015}
+    div.image-container>
+        img.img>scr="project.image">alt="Portfolio Project Image"
+        p.discription <- project.description
+    div.detail-footer >
+        ul.classification > 
+            li.lang> a>href="#" <- project.technologies[i] {Loop}
+    button.btn>type="button">p <- "See Live">img>src..>alt..
+    button.btn>type="button">p <- "See Source">img>src..>alt..
+  */
+}
+
+
+
+// List of project card details
+const defaultDescription = 'A daily section of privately personalized reads; no accounts or sign-ups required.';
+const listProject = [
+  {
+    name: 'Multi-Post Stories Gain+Glory', description: defaultDescription, image: 'images/Project-1.png', technologies: ['Ruby on rails', 'css', 'JavaScript', 'html'], link: { live: '', source: '' },
+  },
+  {
+    name: 'Multi-Post Stories Gain+Glory', description: defaultDescription, image: 'images/Project-1.png', technologies: ['Ruby on rails', 'css', 'JavaScript', 'html'], link: { live: '', source: '' },
+  },
+  {
+    name: 'Multi-Post Stories Gain+Glory', description: defaultDescription, image: 'images/Project-1.png', technologies: ['Ruby on rails', 'css', 'JavaScript', 'html'], link: { live: '', source: '' },
+  },
+  {
+    name: 'Multi-Post Stories Gain+Glory', description: defaultDescription, image: 'images/Project-1.png', technologies: ['Ruby on rails', 'css', 'JavaScript', 'html'], link: { live: '', source: '' },
+  },
+  {
+    name: 'Multi-Post Stories Gain+Glory', description: defaultDescription, image: 'images/Project-1.png', technologies: ['Ruby on rails', 'css', 'JavaScript', 'html'], link: { live: '', source: '' },
+  },
+  {
+    name: 'Multi-Post Stories Gain+Glory', description: defaultDescription, image: 'images/Project-1.png', technologies: ['Ruby on rails', 'css', 'JavaScript', 'html'], link: { live: '', source: '' },
+  },
+];
+
+//Cards Container
+const work = document.getElementById('work');
+let index = 0;
+
+listProject.forEach((project) => {
+  const workContainer = document.createElement('div');
+  workContainer.classList.add('work-card');
+  //div.work-card
+
+  const img = document.createElement('img');
+  img.classList.add('img');
+  img.src = project.image;
+  img.alt = "Project Images";
+  //img.img>src..>alt..
+
+  const title = document.createElement('h2');
+  title.classList.add('project-title');
+  title.textContent = project.name;
+  //h2.project-title
+
+  //Creating list of programming languages
+  const technologies = document.createElement('ul');
+  technologies.classList.add('tag');
+  for(let i = 0 ; i < project.technologies.length; i +=1){
+    const list = document.createElement('li');
+    list.classList.add('lang');
+    list.textContent = project.technologies[i];
+    technologies.append(list);
+  }
+
+  //btn for the popup-detailed project
+  const buttonDiv = document.createElement('div');
+  const button = document.createElement('button');
+  button.classList.add('btn');
+  button.type = 'button';
+  button.textContent = 'See Project';
+
+  //On click event to show details
+  button.addEventListener('click', () => {
+    showDetails(project);
+  });
+
+  buttonDiv.append(button);
+
+  //add all elements
+
+  workContainer.append(img);
+  workContainer.append(title);
+  workContainer.append(technologies);
+  workContainer.append(buttonDiv);
+  
+  //add project to the work container
+  work.append(workContainer)
+  index += 1;
+});
