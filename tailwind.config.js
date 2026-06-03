d1f9cda2d15a4d78339fd38c9b0219e1a834ee51
ahr <ahmed.hasan.rony@gmail.com>
2026-06-03 18:44:49 +0600

    Deploy : Latest
    Signed-off-by: ahr <ahmed.hasan.rony@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
+  content: ["./src/**/*.{js,jsx}"],
   mode: "jit",
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
   theme: {
     extend: {
       colors: {
-        amazing_blue: {
-          light: "#232F3E",
-          DEFAULT: "#131921",
-        },
+        primary: "#050816",
+        secondary: "#aaa6c3",
+        tertiary: "#151030",
+        "black-100": "#100d25",
+        "black-200": "#090325",
+        "white-100": "#f3f3f3",
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "450px",
+      },
+      backgroundImage: {
+        "hero-pattern": "url('/src/assets/herobg.png')",
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 }
